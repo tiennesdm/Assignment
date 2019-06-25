1ad689b1f752aa84ec20a442b5bb7b866e04ab8f
tiennesdm <shubahm.mehta@gmail.com>
2019-06-25 11:17:58 +0530

    fixed api response bug

--- backend/controller/vote.js
+++ backend/controller/vote.js
@@ -12,28 +12,29 @@ exports.postUpvote = (req, res, next) => {
     //   db.inventory.find( { qty: { $exists: true, $nin: [ 5, 15 ] } } )
     Comments.find({ _id: req.body.commentId, creator: req.userData.userId })
         .then((result => {
-            // console.log(result.length);
-            resultlength = result.length;
+            console.log('result', result.length);
+            // resultlength = result.length;
             //  this.resultId = result[0]._id;
             //  this.creatorId = result[0].creator;
             // res.status(201).json({ message: "We cannot process your vote " });
             //  console.log(result[0]._id);
             //  console.log(result[0].creator);
+            if (result.length == 1) {
+                res.status(401).json({ message: "You are creator! We cannot process " });
+
+            } else {
+                postVote(commentId, votingUserId, 'UPVOTE').then(data => {
+                    res.status(201).json(data);
+                }).catch(ex => {
+                    res.status(500).json({
+                        message: "Vote Creation failed"
+                    });
+                })
+
+            }
         })).catch(err => console.log(err));
     //   console.log('resultId', resultlength);
-    if (resultlength == 1) {
-        res.status(500).json({ message: "You are creator! We cannot process " });
 
-    } else {
-        postVote(commentId, votingUserId, 'UPVOTE').then(data => {
-            res.status(201).json(data);
-        }).catch(ex => {
-            res.status(500).json({
-                message: "Vote Creation failed"
-            });
-        })
-
-    }
 
 };
 
@@ -44,25 +45,26 @@ exports.postDownvote = (req, res, next) => {
 
     Comments.find({ _id: req.body.commentId, creator: req.userData.userId })
         .then((result => {
-            resultlength = result.length;
+            //   resultlength = result.length;
             // res.status(201).json({ message: "We cannot process your vote " });
+            if (result.length == 1) {
+                res.status(401).json({ message: "You are creator! We cannot process " });
+
+            } else {
+                postVote(commentId, votingUserId, 'DOWNVOTE').then(data => {
+                    res.status(201).json(data);
+                }).catch(ex => {
+                    res.status(500).json({
+                        message: "Sorry We cannot process the vote"
+                    });
+                })
+
+            }
 
 
             //   console.log(result);
         })).catch(err => console.log(err));
-    if (resultlength == 1) {
-        res.status(500).json({ message: "You are creator! We cannot process " });
 
-    } else {
-        postVote(commentId, votingUserId, 'DOWNVOTE').then(data => {
-            res.status(201).json(data);
-        }).catch(ex => {
-            res.status(500).json({
-                message: "Sorry We cannot process the vote"
-            });
-        })
-
-    }
 
 
 
